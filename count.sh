ebdfccbf02091107f3fbc2fd5a0db82b5b9ef85d
Shahin Saneinejad <shahin.saneinejad@gmail.com>
2014-01-04 22:12:25 -0800

    Fix the example script for count to use secure fifos

--- count.sh
+++ count.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# this is what I want sqltxt.py to generate for the given SQL:
 # select col_a, count(col_b) from table group by col_a;
 
-join -a 2 count_col_b_group_by_col_a_fifo group_by_col_a_fifo | \
+SQLTXT_TMPDIR=$(mktemp -d -t sqltxt_); mkfifo $SQLTXT_TMPDIR/count_col_b_group_by_col_a $SQLTXT_TMPDIR/group_by_col_a || exit 1;
+
+join -a 2 $SQLTXT_TMPDIR/count_col_b_group_by_col_a $SQLTXT_TMPDIR/group_by_col_a | \
 awk -F' ' '{ if($2=="") $2=0; print}' &
 
 echo -e "1,\n2,r\n2,b\n3,b" |\
@@ -12,7 +15,9 @@ tee >(\
   sort -t, -k 1.1 |\
   uniq -c |\
   awk -F' ' '{ print $2, $1 }' \
-  > count_col_b_group_by_col_a_fifo) |\
+  > $SQLTXT_TMPDIR/count_col_b_group_by_col_a) |\
 cut -d, -f1 |\
 sort -t, -k 1.1 -u \
-> group_by_col_a_fifo
+> $SQLTXT_TMPDIR/group_by_col_a;
+
+rm -rf $SQLTXT_TMPDIR
